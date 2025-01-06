2ada63b128308b0465e8900fd592f92d85bcedc7
EnriqueOrtiz2001 <enriquotero@gmail.com>
2025-01-05 23:03:14 -0500

    route2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 import React from 'react';
 import Spline from '@splinetool/react-spline';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 
 const Page = () => {
  
